a78eed1d17dcb8ff4f56c30185bf3be5765ab167
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2026-08-19 22:01:11 +0100

    Moving forward/back word keys to shared file, adding dotenv plugin and required env var

--- home/dot_zsh/antidote_config.zsh
+++ home/dot_zsh/antidote_config.zsh
@@ -2,5 +2,8 @@
 
 source $(brew --prefix)/opt/antidote/share/antidote/antidote.zsh
 
+# Used for several oh-my-zsh plugins
+ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/zsh"
+
 # initialize plugins statically with ${ZDOTDIR:-$HOME}/.zsh_plugins.txt
 antidote load
