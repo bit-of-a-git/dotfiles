355bceab75696f9d0575c93cb046d426f7c10c83
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2026-08-29 23:15:34 +0100

    Enabling fzf-tab completion, moving antidote file and putting lines in .zshrc

--- home/dot_config/zsh/antidote_config.zsh
+++
@@ -1,6 +0,0 @@
-# ~/.antidote-config.zsh
-
-source $(brew --prefix)/opt/antidote/share/antidote/antidote.zsh
-
-# initialize plugins statically with ${ZDOTDIR:-$HOME}/.zsh_plugins.txt
-antidote load
