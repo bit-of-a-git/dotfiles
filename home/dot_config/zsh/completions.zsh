8ecce0d97c66260b92df1f078c8c71a7780885ce
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2026-08-30 00:29:23 +0100

    Adding fzf-tab completion stuff

--- home/dot_config/zsh/completions.zsh
+++ home/dot_config/zsh/completions.zsh
@@ -8,11 +8,31 @@ export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
 zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
 source <(carapace _carapace)
 
-# Better colors for file listings in completion
-zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no
 
-zstyle ':completion:*' list-prompt ''
+# set descriptions format to enable group support
+# NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
+zstyle ':completion:*:descriptions' format '[%d]'
 
-# Menu selection with arrow keys and visual highlighting
-zstyle ':completion:*' menu select
-zstyle ':completion:*' select-prompt ' %SScrolling active: current selection at %p%s'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# preview directory's content when completing cd
+if ! command -v eza >/dev/null 2>&1; then
+	zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+else
+	zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls -1 --color=always $realpath'
+fi
+
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+
+# custom fzf flags
+# NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
+zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
+# To make fzf-tab follow FZF_DEFAULT_OPTS.
+# NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
+# switch group using `<` and `>`
+zstyle ':fzf-tab:*' switch-group '<' '>'
