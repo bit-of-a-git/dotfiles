043da8d5a44022b760d2c6e060480a5a1c630cd0
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2026-08-30 17:22:29 +0100

    Removing carapace from zsh for now

--- home/dot_config/zsh/completions.zsh
+++ home/dot_config/zsh/completions.zsh
@@ -3,11 +3,6 @@
 # Initialize Zsh completion system
 autoload -U compinit && compinit
 
-# Carapace-related
-export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
-zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
-source <(carapace _carapace)
-
 # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
 zstyle ':completion:*' menu no
 
@@ -19,7 +14,7 @@ zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 # preview directory's content when completing cd
-if ! command -v eza >/dev/null 2>&1; then
+if command -v eza >/dev/null 2>&1; then
 	zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
 else
 	zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls -1 --color=always $realpath'
@@ -30,7 +25,7 @@ zstyle ':completion:*:git-checkout:*' sort false
 
 # custom fzf flags
 # NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
-zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
+zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=enter:accept
 # To make fzf-tab follow FZF_DEFAULT_OPTS.
 # NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
 zstyle ':fzf-tab:*' use-fzf-default-opts yes
